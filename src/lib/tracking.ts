bdb75cbff58b5c54b9dd9297068639580c794e48
Sergio <tu-email@ejemplo.com>
2025-05-02 21:43:08 -0500

    a

--- src/lib/tracking.ts
+++ src/lib/tracking.ts
@@ -1,16 +1,17 @@
 export const trackConversion = (eventName: string, eventData = {}) => {
-    if (typeof window !== 'undefined') {
-      // Google Analytics
-      if (window.gtag) {
-        window.gtag('event', eventName, {
-          'send_to': 'AW-697508779/tF-ACKqxyb4aEKvHzMwC',
-          ...eventData
-        });
-      }
-      
-      // Facebook Pixel
-      if (window.fbq) {
-        window.fbq('track', eventName, eventData);
-      }
+  if (typeof window !== 'undefined') {
+    // Google Analytics
+    const w = window as any;
+    if (w.gtag) {
+      w.gtag('event', eventName, {
+        'send_to': 'AW-697508779/tF-ACKqxyb4aEKvHzMwC',
+        ...eventData
+      });
     }
-  };
+    
+    // Facebook Pixel
+    if (w.fbq) {
+      w.fbq('track', eventName, eventData);
+    }
+  }
+};
