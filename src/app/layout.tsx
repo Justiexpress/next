acc3613616a82b4684cded1fdf841b0683e1130b
Sergio <tu-email@ejemplo.com>
2025-05-04 17:13:50 -0500

    TagManager

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -45,7 +45,19 @@ export default function RootLayout({
   return (
     <html lang="es">
       <head>
-        {/* Google Analytics */}
+        {/* Google Tag Manager */}
+        <Script id="google-tagmanager" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-T9B26PB');
+          `}
+        </Script>
+        {/* Fin Google Tag Manager */}
+
+        {/* Mantenemos Google Analytics por compatibilidad */}
         <Script
           src="https://www.googletagmanager.com/gtag/js?id=G-0RH4X36HRM"
           strategy="afterInteractive"
@@ -65,6 +77,18 @@ export default function RootLayout({
         <link rel="icon" href="/favicon.ico" />
       </head>
       <body>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe 
+            src="https://www.googletagmanager.com/ns.html?id=GTM-T9B26PB"
+            height="0" 
+            width="0" 
+            style={{ display: 'none', visibility: 'hidden' }}
+            title="Google Tag Manager"
+          />
+        </noscript>
+        {/* Fin Google Tag Manager (noscript) */}
+        
         <div className="app-container" itemScope itemType="https://schema.org/LegalService">
           <meta itemProp="name" content="Justiexpress - Servicios Legales Online" />
           <meta itemProp="description" content="Servicios legales profesionales en Colombia: tutelas, derechos de petición y asesoría jurídica" />
