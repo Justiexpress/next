859a1f778e3bb6dc10481d6ebd7ebbef46c4a1fb
Sergio <tu-email@ejemplo.com>
2025-05-04 21:04:23 -0500

    Tracking

--- src/lib/tracking.ts
+++ src/lib/tracking.ts
@@ -1,6 +1,14 @@
 export const trackConversion = (eventName: string, eventData = {}) => {
   if (typeof window !== 'undefined') {
-    // Google Analytics
+    // Enviar a la capa de datos de GTM
+    if ((window as any).dataLayer) {
+      (window as any).dataLayer.push({
+        event: eventName,
+        ...eventData
+      });
+    }
+    
+    // Google Ads
     const w = window as any;
     if (w.gtag) {
       w.gtag('event', eventName, {
